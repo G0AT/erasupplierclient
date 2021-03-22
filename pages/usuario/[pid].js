340f0618bdbc0ba6604e1ecba498b0a5b478547f
programador.gdl@enginetsystems.com <programador.gdl@enginetsystems.com>
2021-03-22 01:39:38 -0600

    test

--- pages/usuario/[pid].js
+++ pages/usuario/[pid].js
@@ -208,7 +208,7 @@ const EditarUsuario = () => {
                                     ) : null }
 
                                 <div className="mb-4">
-                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password Usuario</label>
+                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
                                     <input 
                                         type="password" 
                                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-400 focus:shadow-outline "
